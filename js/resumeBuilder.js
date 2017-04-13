e1e711741f7b322c57ea2104c18f74f0418344e6
唐和辉 <yourgitemail@example.com>
2017-04-13 21:44:52 +0800

    英文简历分支

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,25 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var awesomeThoughts = "My name is Douglas and I am AWESOME!";
- var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
- $("#main").append(funThoughts);
- var formattedName, formattedRole;
- formattedName = HTMLheaderName.replace("%data%", "Douglas");
- var role = "Front-End engineer";
- formattedRole = HTMLheaderRole.replace("%data%", role);
- //让添加字符串在header类的最上层
+var formattedName, formattedRole;
+formattedName = HTMLheaderName.replace("%data%", "Douglas");
+var role = "Front-End engineer";
+formattedRole = HTMLheaderRole.replace("%data%", role);
+//让添加字符串在header类的最上层
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+var bio = {
+    "name" : "Douglas",
+    "role" : "Front-End engineer／UI Designer",
+    "contact" : {
+        "mobile" : "18999509255",
+        "email" : "ad546971975@gmail.com",
+        "location" : "Urumuqi"
+    },
+    "welcomeMessage" : "lorem ipsum sit amet etc",
+    "skills": [
+        "Javascript Coding", "Mobile Design", "Web Design", "prototype Design"
+    ],
+    "bioPic" : "image/Avater.jpg"
+};
