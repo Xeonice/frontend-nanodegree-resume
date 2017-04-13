478d0552df06d1fd2e3f4233e26d7690d0fa687c
唐和辉 <yourgitemail@example.com>
2017-04-13 17:17:48 +0800

    练习: String.Replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts = "My name is Douglas and I am AWESOME!";
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ $("#main").append(funThoughts);
