d7ee81cd6f0542edded164498a148a7a9473d878
唐和辉 <yourgitemail@example.com>
2017-04-13 17:50:01 +0800

    练习: 格式化数据，生成简历标题

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,3 +4,11 @@ This is empty on purpose! Your code to build the resume will go here.
  var awesomeThoughts = "My name is Douglas and I am AWESOME!";
  var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
  $("#main").append(funThoughts);
+ var formattedName, formattedRole;
+ formattedName = HTMLheaderName.replace("%data%", "Douglas");
+ var role = "Front-End engineer";
+ formattedRole = HTMLheaderRole.replace("%data%", role);
+ //让添加字符串在header类的最上层
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
