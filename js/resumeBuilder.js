8f670168fa75394fb0365fe2b0ee5c78733d640b
唐和辉 <yourgitemail@example.com>
2017-04-20 19:42:10 +0800

    项目第二版（根据Code Review进行了删改） 注：Online Course问题依旧没有得到修改

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,168 +4,164 @@
 var bio = {
     "name": "Douglas",
     "role": "Front-End engineer／UI Designer",
-    "contacts": [{
+    "contacts": {
         "mobile": "18999509255",
         "email": "ad546971975@gmail.com",
-        "location": "Urumuqi"
-    }],
+        "location": "No.499,Xibei Road,Saybagh District,Urumqi",
+        "github": "Xeonice"
+    },
     "welcomeMessage": "lorem ipsum sit amet etc",
     "skills": [
         "Javascript Coding", "Mobile Design", "Web Design", "prototype Design"
     ],
-    "bioPic": ["images/avater.jpg"]
-}
+    "biopic": ["images/avater.jpg"]
+};
 var work = {
     "jobs": [{
-        "location": "Urumuqi",
+        "location": "No.516,Xibei Road,Saybagh District,Urumqi",
         "title": "trainee",
         "employer": "Cloud Calculate Xinjiang",
         "dates": "in progress",
         "description": "My first job in software company."
     },
         {
-            "location": "Urumuqi",
+            "location": "No.16,Linyuan Road,Karamary City",
             "title": "Seller",
             "employer": "Pioneer Sports Xinjiang",
             "dates": "1 years",
             "description": "To sell sport productions."
         }
     ]
-}
+};
 
 var education = {
     "schools": [{
         "name": "Xinjiang University",
-        "position": "Urumuqi",
+        "location": "No.499,Xibei Road,Saybagh District,Urumqi",
         "degree": "masters",
-        "dates": 2019,
-        "urls": "www.xju.edu.cn/",
-        "major": ["Software"]
+        "dates": "2019",
+        "urls": "http://www.xju.edu.cn/",
+        "majors": ["Software"]
     }],
     "onlineCourses": [{
         "title": "FEND engineer",
         "school": "Udacity",
-        "dates": 2017,
+        "dates": "2017",
         "url": "http://example.com"
     }]
 };
 var projects = {
-    "Projects": [{
-        "titles": "Adam Design UI Design",
-        "dates": 2016,
+    "project1": [
+        {
+        "title": "Adam Design UI Design",
+        "dates": "2016",
         "description": "Adam Design's UI Mobile Design",
         "images": ["images/Adam_Design_UI.jpg"]
-    },
+        },
         {
-            "titles": "Adam Design Powerpoing Design",
-            "dates": 2017,
+            "title": "Adam Design Powerpoing Design",
+            "dates": "2017",
             "description": "Adam Design's Powerpoints Design",
             "images": ["images/Adam_Design_PPT.jpg"]
         }
     ]
 };
-//添加姓名
 bio.display = function() {
-    //添加姓名，职位，图片，欢迎信息
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedImage = HTMLbioPic.replace("%data%", bio.image);
+    var formattedImage = HTMLbioPic.replace("%data%", bio.biopic);
     var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
     $("#header").prepend(formattedRole).prepend(formattedName).append(formattedImage, formattedMessage);
     $("#header").append(HTMLskillsStart);
-    //添加联系信息
-    for (contactCount = 0; contactCount < bio.contacts.length; contactCount++) {
-        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[contactCount].mobile);
-        var formattedEmail = HTMLemail.replace("%data%", bio.contacts[contactCount].email);
-        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts[contactCount].location);
-        $("#topContacts").append(formattedMobile, formattedEmail, formattedLocation);
-    };
-    //添加工作技能
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts,#footerContacts").append(formattedMobile, formattedEmail, formattedLocation, formattedGithub);
     for (skillCount = 0; skillCount < bio.skills.length; skillCount++) {
         var formattedSkill = HTMLskills.replace("%data%", bio.skills[skillCount]);
         $("#skills").append(formattedSkill);
     };
 };
-bio.display();
 
-//添加工作经历
 work.display = function() {
-    for (job = 0; job < work.jobs.length; job++) {
+    for (jobCount = 0; jobCount < work.jobs.length; jobCount++) {
         $("#workExperience").append(HTMLworkStart);
 
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobCount].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[jobCount].title);
         var formattedEmployerTitle = formattedEmployer + formattedTitle;
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[jobCount].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[jobCount].description);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[jobCount].location);
 
-        $(".work-entry:last").append(formattedEmployerTitle, formattedDates, formattedDescription);
+        $(".work-entry:last").append(formattedEmployerTitle, formattedLocation, formattedDates, formattedDescription);
     }
 };
-work.display()
 
 education.display = function() {
+    $("#education").append(HTMLschoolStart);
     for(schoolCount = 0; schoolCount < education.schools.length; schoolCount++){
-        $("#education").append(HTMLschoolStart);
 
         var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[schoolCount].name);
-        var formattedPosition = HTMLschoolLocation.replace("%data%", education.schools[schoolCount].position);
+        var formattedPosition = HTMLschoolLocation.replace("%data%", education.schools[schoolCount].location);
         var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[schoolCount].degree);
-        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[schoolCount].major);
+        var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[schoolCount].majors);
         var formattedDate = HTMLschoolDates.replace("%data%", education.schools[schoolCount].dates);
 
-        $(".education-entry:last").append(formattedSchoolName+formattedDegree,formattedDate, formattedPosition, formattedMajor );
+        $(".education-entry:last").append(formattedSchoolName+formattedDegree,formattedDate, formattedPosition, formattedMajors );
     }
+    $("#education").append(HTMLschoolStart);
+    $("#education").append(HTMLonlineClasses);
     for(onlineCount = 0; onlineCount < education.onlineCourses.length; onlineCount++){
-        $("#education").append(HTMLonlineClasses);
         var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCount].title);
         var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCount].school);
         var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCount].dates);
         var formattedURL = HTMLonlineURL.replace("%data", education.onlineCourses[onlineCount].url);
 
-        $(".onlineCourse-entry:last").append(formattedTitle+formattedSchool, formattedDates, formattedURL);
+        $(".education-entry:last").append(formattedTitle+formattedSchool, formattedDates, formattedURL);
     }
 
 };
-education.display();
 
-//鼠标追踪函数（在控制台输出X,Y坐标）
-$(document).click(function(loc) {
-    var x = loc.pageX;
-    var y = loc.pageY;
+// $(document).click(function(loc) {
+//     var x = loc.pageX;
+//     var y = loc.pageY;
+//
+//     logClicks(x, y);
+// });
+// function inname(name) {
+//     name = name.trim().split(" ");
+//     console.log(name);
+//     name[1] = name[1].toUpperCase();
+//     name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+//     return name[0] + " " + name[1];
+// }
+// $("#main").append(internationalizeButton);
 
-    logClicks(x, y);
-});
-//国际化（姓大写，名首字母大写）函数
-function inname(name) {
-    name = name.trim().split(" ");
-    console.log(name);
-    name[1] = name[1].toUpperCase();
-    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-    return name[0] + " " + name[1];
-}
-$("#main").append(internationalizeButton);
-
-//添加项目信息（封装）
 projects.display = function() {
-    for (count in projects.Projects) {
+    for (count = 0; count <  projects.project1.length; count++) {
         $("#projects").append(HTMLprojectStart);
 
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.Projects[count].titles);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project1[count].title);
         $(".project-entry:last").append(formattedTitle);
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.Projects[count].dates);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.project1[count].dates);
         $(".project-entry:last").append(formattedDates);
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.Projects[count].description);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project1[count].description);
         $(".project-entry:last").append(formattedDescription);
 
-        if (projects.Projects[count].images.length > 0) {
-            for (image in projects.Projects[count].images) {
-                var formattedImages = HTMLprojectImage.replace("%data%", projects.Projects[count].images)
+        if (projects.project1[count].images.length > 0) {
+            for (image in projects.project1[count].images) {
+                var formattedImages = HTMLprojectImage.replace("%data%", projects.project1[count].images)
                 $(".project-entry:last").append(formattedImages);
             }
         }
     }
 }
+bio.display();
+work.display()
+education.display();
 projects.display();
 $("#mapDiv").append(googleMap);
+
